0eaa474957d36d4a29a598fc9e89d1d1254eb0d4
Yana Knysh <jemelachee@gmail.com>
2023-05-22 12:14:10 +0300

    update TCPSender

--- TcpSender/Program.cs
+++ TcpSender/Program.cs
@@ -14,14 +14,28 @@ class MyTcpSender
 
     public static async Task SendRequests()
     {
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.GetStatus.ToString());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last());
-        await TcpSender.Connect("127.0.0.1", 1000, Command.GetStatus.ToString());
-        await TcpSender.Connect("127.0.0.1", 1000, "hello3");
+        var list = new List<Tuple<string, int, string>>()
+        {
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.GetStatus.ToString()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.First()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.Print.ToString() + Label.labels.Last()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, Command.GetStatus.ToString()),
+            new Tuple<string, int, string>("127.0.0.1", 1000, "hello3"),
+
+        };
+
+        ParallelOptions parallelOptions = new()
+        {
+            MaxDegreeOfParallelism = 3
+        };
+        await Parallel.ForEachAsync(list, async (item, CancellationToken) =>
+        {
+            await TcpSender.Connect(item.Item1, item.Item2, item.Item3);
+        });
     }
+
 }
